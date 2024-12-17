296510835b42c50e67346c2e91e42e8e73e77d89
radioation <greggallardo@gmail.com>
2024-12-17 00:20:49 -0600

    Not pretty, but peer connection working on LAN

--- 02_simple_peers/src/main.c
+++ 02_simple_peers/src/main.c
@@ -4,6 +4,35 @@
 int cursor_x, cursor_y;
 u8 buttons, buttons_prev;
 
+void host_game() {
+    // Allow client to join 
+    NET_allowConnections();
+    VDP_drawText("           Waiting       ", 0, 5);
+
+    // loop while waiting for a peer.
+    u8 offset = 0;
+    while( 1 ) {
+        // Data available?
+        while( !NET_RXReady() ){
+            // writing some sort of text here?
+            VDP_drawText(" .     ", 21+offset, 5);
+            offset +=1;
+            if( offset > 3 ) offset = 0;
+            waitMs(16);
+
+        }
+        // look for 'C'
+        u8 ret = NET_readByte();
+        if( ret == 'C' ) {
+            // clear text before losing out
+            VDP_drawText("          Connected!     ", 0, 5);
+            return;
+        }
+    }
+
+}
+
+
 int main()
 {
     SYS_disableInts();                      // Disable interrupts
@@ -39,9 +68,6 @@ int main()
         while(1) { SYS_doVBlankProcess(); }
     }
 
-//------------------------------------------------------------------
-// MAIN LOOP
-//------------------------------------------------------------------
 
     VDP_drawText("IP Address:", cursor_x, cursor_y); 
     NET_printIP(cursor_x+12, cursor_y); cursor_y++;
@@ -49,69 +75,94 @@ int main()
     VDP_drawText("MAC:", cursor_x, cursor_y); 
     NET_printMAC(cursor_x+5, cursor_y); cursor_y+=2;
 
-    waitMs(2000);
-
-    NET_pingIP(cursor_x, cursor_y, 4, "10.86.22.6"); cursor_y+=6;
 
     waitMs(2000);
 
 
+    for(int i=5; i < 13; ++i ) {
+        VDP_drawText("                                   ", 0, i);
+    }
+    VDP_drawText("          (A) - Host Game", 0, 5);
+    VDP_drawText("          (C) - Join Game", 0, 7);
+    NET_resetAdapter();
 
-    u8 mode = 0; // 0 - not set, 1 - server, 2 - client
+    u8 mode = 0; // 0 - not set, 1 - Host, 2 - Client
+
+    //------------------------------------------------------------------
+    // MAIN LOOP
+    //------------------------------------------------------------------
     while(1) // Loop forever 
     { 
+        buttons = JOY_readJoypad(JOY_1);
+        if( mode == 0 ) {
+            // MODE NOT SET, button press will determine server or client.
+            if(buttons & BUTTON_A && buttons_prev == 0x00) { 
+                VDP_drawText("                         ", 0, 5);
+                VDP_drawText("                         ", 0, 7);
+                cursor_y = 5;
+                mode = 1;
+                // start listening 
+                host_game();
 
+            }else if(buttons & BUTTON_C && buttons_prev == 0x00) { 
+                VDP_drawText("                         ", 0, 5);
+                VDP_drawText("                         ", 0, 7);
+                // try to connect to server.
+                mode = 2;
+                cursor_y = 5;
+                NET_connect(cursor_x, cursor_y, "010.086.022.036:5364"); cursor_x=0; cursor_y++;
 
-	    buttons = JOY_readJoypad(JOY_1);
-	    if( mode == 0 ) {
+            }
+        } else if ( mode == 1 ) {
+            // HOST mode
+            if(buttons & BUTTON_START && buttons_prev == 0x00) { NET_sendMessage("SH"); }
+            if(buttons & BUTTON_A && buttons_prev == 0x00) { 
+                if( NET_TXReady() ) {
+                    NET_sendByte('A');
+                    NET_sendByte('H');
+                }
+            }
+            if(buttons & BUTTON_B && buttons_prev == 0x00) { NET_sendMessage("BH"); }
+            if(buttons & BUTTON_C && buttons_prev == 0x00) { NET_sendMessage("CH"); }
+        } else if ( mode == 2 ) {
+            // Client mode
+            if(buttons & BUTTON_START && buttons_prev == 0x00) { NET_sendMessage("SC"); }
+            if(buttons & BUTTON_A && buttons_prev == 0x00) {
+                if( NET_TXReady() ) {
+                    NET_sendByte('A');
+                    NET_sendByte('C');
+                }
+            }
+            if(buttons & BUTTON_B && buttons_prev == 0x00) { NET_sendMessage("BC"); }
+            if(buttons & BUTTON_C && buttons_prev == 0x00) { NET_sendMessage("CC"); }
+        }
 
-		    if(buttons & BUTTON_A && buttons_prev == 0x00) { 
-			    mode = 1;
-			    // start listening
-
-		    }else if(buttons & BUTTON_C && buttons_prev == 0x00) { 
-			    // try to connect to server.
-			    mode = 2;
-			    VDP_drawText("Rebooting adapter...", cursor_x ,cursor_y); cursor_y+=2;
-			    NET_resetAdapter();
-
-			    NET_connect(cursor_x, cursor_y, "10.86.22.6:8080"); cursor_x=0; cursor_y++;
-		    }
-	    } else if ( mode == 1 ) {
-	    } else if ( mode == 2 ) {
-		    if(buttons & BUTTON_START && buttons_prev == 0x00) { NET_sendMessage("Test 1, 2, 3\n"); }
-		    if(buttons & BUTTON_A && buttons_prev == 0x00) { NET_sendMessage("Button A Pressed\n"); }
-		    if(buttons & BUTTON_B && buttons_prev == 0x00) { NET_sendMessage("Button B Pressed\n"); }
-		    if(buttons & BUTTON_C && buttons_prev == 0x00) { NET_sendMessage("Button C Pressed\n"); }
-	    }
-	    while(NET_RXReady()) // while data in hardware receive FIFO
-	    {   
-		    u8 byte = NET_readByte(); // Retrieve byte from RX hardware Fifo directly
-		    switch(byte)
-		    {
-			    case 0x0A: // a line feed?
-				    cursor_y++;
-				    cursor_x=1;
-				    break;              
-			    case 0x0D: // a carridge Return?
-				    cursor_x=1;
-				    break; 
-			    default:   // print
-				    if (cursor_x >= 40) { cursor_x=0; cursor_y++; }
-				    if (cursor_y >= 28) { cursor_x=0; cursor_y=0; }
-				    sprintf(str, "%c", byte); // Convert
-				    VDP_drawText(str, cursor_x, cursor_y); cursor_x++;
-				    break;
-		    }
-	    }
-	    buttons_prev = buttons;
-	    SYS_doVBlankProcess(); 
+        while(NET_RXReady()) // while data in hardware receive FIFO
+        {   
+            u8 byte = NET_readByte(); // Retrieve byte from RX hardware Fifo directly
+            switch(byte)
+            {
+                case 0x0A: // a line feed?
+                    cursor_y++;
+                    cursor_x=1;
+                    break;              
+                case 0x0D: // a carridge Return?
+                    cursor_x=1;
+                    break; 
+                default:   // print
+                    if (cursor_x >= 40) { cursor_x=0; cursor_y++; }
+                    if (cursor_y >= 28) { cursor_x=0; cursor_y=0; }
+                    sprintf(str, "%c", byte); // Convert
+                    VDP_drawText(str, cursor_x, cursor_y); cursor_x++;
+                    break;
+            }
+        }
+        buttons_prev = buttons;
+        SYS_doVBlankProcess(); 
     }
 
     //------------------------------------------------------------------
     return(0);
 }
-
-
 
 
